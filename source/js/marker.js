e58e694e34d8f934c6e9eb8faf23ebab7b9921ab
masha8709 <masha.8709@mail.ru>
2021-05-20 14:59:13 +0300

    Добавила ключ

--- source/js/marker.js
+++ source/js/marker.js
@@ -1,7 +1,13 @@
 function initMap() {
-  const image = document.querySelector('.contacts__map-marker');
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 18,
+    center: { lat: 30.321008, lng: 59.9387809 },
+  });
+  const image =
+    "img/svg/map-marker.svg";
   const beachMarker = new google.maps.Marker({
-    position: { lat: -38, lng: 151.274 },
+    position: { lat: 30.321008, lng: 59.9387809 },
+    map,
     icon: image,
   });
 }
