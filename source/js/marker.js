63a121235d995740ffe9195c1f9c5b909372e149
masha8709 <masha.8709@mail.ru>
2021-05-20 20:56:37 +0300

    Пиксель пёрфект для главной страницы

--- source/js/marker.js
+++ source/js/marker.js
@@ -1,12 +1,12 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 18,
-    center: { lat: 30.321008, lng: 59.9387809 },
+    center: { lat: 59.8909887, lng: 30.5316441 },
   });
   const image =
     "img/svg/map-marker.svg";
   const beachMarker = new google.maps.Marker({
-    position: { lat: 30.321008, lng: 59.9387809 },
+    position: { lat: 59.8909887, lng: 30.5316441 },
     map,
     icon: image,
   });
